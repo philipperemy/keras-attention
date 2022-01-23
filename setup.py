08095bfdd0b0e9ea706d5480f42083936a5663a6
Philippe Rémy <premy.enseirb@gmail.com>
2022-01-23 21:35:15 +0900

    Update lib + misc (#63)
    * update attention
    
    * update attention
    
    * small improvements
    
    * CI
    
    * CI
    
    * 4.1
    
    * update attention
    
    * update attention
    
    * update attention
    
    * update attention

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='attention',
-    version='4.0',
+    version='4.1',
     description='Keras Simple Attention',
     author='Philippe Remy',
     license='Apache 2.0',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
-    packages=['attention'],
+    packages=find_packages(),
     install_requires=[
         'numpy>=1.18.1',
         'tensorflow>=2.1'
